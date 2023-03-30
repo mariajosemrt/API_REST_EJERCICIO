0f4a53f1c7b66287d260fbd78e06a3dcae492d4e
mariajosemrt <andreumj7@gmail.com>
2023-03-30 14:41:52 +0200

    Inicio proyecto

---
+++ src/main/java/com/ejercicio/ApiRestEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEjercicioApplication.class, args);
+	}
+
+}
